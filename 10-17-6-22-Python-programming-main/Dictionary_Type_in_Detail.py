f5a5627e5bdfe91df5dfb23d1345887d509d59cd
Khan <pathan-nayum.khan@capgemini.com>
2022-06-21 23:12:36 +0530

    Added code for 21_June

---
+++ 10-17-6-22-Python-programming-main/Dictionary_Type_in_Detail.py
@@ -0,0 +1,46 @@
+# h={1:"",2: "Python", 3: 7, 4: 6.2,
+# 5: [1,4], 6: (4,3), 7: {1,2},
+# 8: {3: "world"}}
+# print(h[1],type(h[1]))
+# print(h[2],type(h[2]))
+# print(h,type(h))
+# print(h.keys())
+# print(h.values())
+# print(h.items())
+
+# h.clear()
+# print(h)
+
+# a=[101,102,103,104,105]
+# b=dict.fromkeys(a)
+# print(b,type(b))
+# print(dict.fromkeys(a,'Python'))
+
+# h={1:"", 2: "Python", 3: 7, 4: 6.2,
+# 5: [1,4], 6: (4,3), 7: {1,2},
+# 8: {3: "world"}}
+# print(h.get(2))
+# print(h[2])
+# h.pop(3)
+# print(h)
+# h.popitem()
+# print(h)
+
+# h={1:"", 2: "Python", 3: 7, 4: 6.2,
+# 5: [1,4], 6: (4,3), 7: {1,2},8: {3: "world"}}
+
+# h.update({8: 'Core Python'})
+# print(h)
+# h.update({9: 'Adv Python'})
+# print(h)
+
+# h.setdefault(8,' Devops')
+# h.setdefault(10,' Devops')
+# print(h)
+
+# print(dir(dict))
+
+# print(len(['clear', 'copy', 'fromkeys',
+#  'get', 'items', 'keys', 'pop', 'popitem',
+#   'setdefault', 'update', 'values']))
+
---
+++ 10-17-6-22-Python-programming-main/pre_defined_func.py
@@ -0,0 +1,37 @@
+'''   pre-defined-functions   '''
+'''    sorted()    '''
+# print(sorted([12,2,120,9]))
+# print(sorted([12,3,4,56]))
+'''     all()     '''
+# print(all([True,True,1,2]))
+# print(all([True,False,1,2]))
+# print(all([True,True,1,2,None]))
+'''     any()     '''
+# print(any([True,False,False,23]))
+# print(any([False,False,0]))
+# print(any([True,True,None,0]))
+'''     bool()     '''
+# print('eval=',eval('5+6.7-1'))
+# a=eval('5+6-1')
+# b=eval('4+3.8-1')
+# print(a,type(a))
+# print(b,type(b))
+'''     sum()     '''
+# print('sum=',sum([1,2,3,4]))
+# print('sum=',sum([10.5,20,30]))
+'''     reversed()-list     '''
+# for i in reversed([1,2,3,4],):
+#     print(i)
+'''     reversed()-tuple     '''
+# for i in reversed((10,20,30,40)):
+#     print(i)
+'''      enumerate()      '''
+# a=["bread","milk","abcd"]
+# b=enumerate(a)
+# print(type(b))
+# print(dict(b))
+# print(list(b))
+# print(tuple(b))
+# c=enumerate(a,5)
+# print(list(c))
+
